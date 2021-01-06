5f5d92b8a8c554d903049b1b0253d1c38cec4d27
josephaw1022 <josephsims1@gmail.com>
2021-01-06 07:29:43 -0500

    🍉 Fixed gitignore message

---
+++ build/precache-manifest.ea5eddde166b21ba0d48a07895723a19.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36d9af2568b7c3be7a14a34d32515b79",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e57296d7fe78cef50677",
+    "url": "/static/css/main.73b0eb39.chunk.css"
+  },
+  {
+    "revision": "f5a403b9e93af55c5f89",
+    "url": "/static/js/2.02bdda3d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.02bdda3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60c82a84e63ae1434422",
+    "url": "/static/js/3.cd00c3fe.chunk.js"
+  },
+  {
+    "revision": "e57296d7fe78cef50677",
+    "url": "/static/js/main.6aed7585.chunk.js"
+  },
+  {
+    "revision": "da172d5c3fc5da2134a8",
+    "url": "/static/js/runtime-main.c3d0d640.js"
+  },
+  {
+    "revision": "80954654631af8ffb5ef4453a9caa3ec",
+    "url": "/static/media/bruno-abatti.80954654.jpg"
+  },
+  {
+    "revision": "ae11d928aae96245020bb59a1910f758",
+    "url": "/static/media/clem-onojeghuo-3.ae11d928.jpg"
+  },
+  {
+    "revision": "2051e0e847124c536df9c01a3f672032",
+    "url": "/static/media/erik-lucatero-2.2051e0e8.jpg"
+  },
+  {
+    "revision": "76ff47ae5c64ccda5af18326bb272058",
+    "url": "/static/media/joe-gardner-2.76ff47ae.jpg"
+  }
+]);

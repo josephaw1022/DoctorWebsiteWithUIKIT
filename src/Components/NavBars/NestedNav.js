a20420e953bd5a433da56d3e3047bcb8f4bf87f5
josephaw1022 <josephsims1@gmail.com>
2021-02-05 18:24:47 -0500

    fixed the navmenu

--- src/Components/NavBars/NestedNav.js
+++ src/Components/NavBars/NestedNav.js
@@ -1,6 +1,8 @@
-import React from "react"
+import React  from "react"
 import "./NAV.json" 
 import "./NavBar.css" 
+import { Link } from "react-router-dom" 
+import {DropdownItem} from "reactstrap" 
 
 class NestedNav extends React.Component{ 
 
@@ -10,16 +12,38 @@ class NestedNav extends React.Component{
             menu: {}  
         }
     }
-    
+
+    OpenJsonFile(){ 
+        const file = require("./NAV.json")
+        this.setState({ menu: file.menu.list})
+    } 
+
+    componentWillMount(){ 
+        this.OpenJsonFile() ; 
+    }
 
     render(){ 
 
+        let MenuPieces = this.state.menu.map( 
+            (index) => { 
+                return( 
+                    <>
+                        <DropdownItem to={index.Link} tag={Link}>
+                            <i className={"nc-icon "+ index.Icon} />
+                            {index.Name}
+                        </DropdownItem>
+                    </> 
+                ); 
+            }
+        )
 
         return(
-            
-
+            <>
+            {MenuPieces}
+            </> 
         ) ; 
-
 
     }
 }
+
+export default NestedNav ; 

a51f5916e629dbdc4da01f886b0f721f4e807c55
josephaw1022 <josephsims1@gmail.com>
2021-04-21 11:32:18 -0400

    formatting

--- src/App/Components/Layout.js
+++ src/App/Components/Layout.js
@@ -1,16 +1,15 @@
-import React, { Component } from "react"
+import React from "react"
 import DemoFooter from "./Footers/DemoFooter"
 import NavBar2 from "./NavBars/NavBar2"
 import NavBarIndex from "./NavBars/NavBarIndex"
 import $ from "jquery"
-export class Layout extends Component {
-    render() {
-        return (
-            <>
-                {$(window).width() > 900 ? <NavBarIndex /> : <NavBar2 />}
-                {this.props.children}
-                <DemoFooter />
-            </>
-        )
-    }
+
+export const Layout = (props) => {
+    return (
+        <>
+            {$(window).width() > 900 ? <NavBarIndex /> : <NavBar2 />}
+            {props.children}
+            <DemoFooter />
+        </>
+    )
 }
